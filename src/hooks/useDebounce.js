8e09e6f12217f1d38d1e8a49fddc71daa1ade9f4
pTn-3001 <trungnguyenphan3001@gmail.com>
2025-06-11 10:06:22 +0700

    Sử dụng axios để gọi API

--- src/hooks/useDebounce.js
+++ src/hooks/useDebounce.js
@@ -7,7 +7,10 @@ function useDebounce(value, delay) {
         const handler = setTimeout(() => {
             setDebouncedValue(value);
         }, delay);
+
         return () => clearTimeout(handler);
+        
+    //eslint-disable-next-line react-hooks/exhaustive-deps
     }, [value]);
 
     return debouncedValue;
