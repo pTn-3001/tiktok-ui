99c93dce2800a2e25bf260bd8480dc39c05f0888
pTn-3001 <trungnguyenphan3001@gmail.com>
2025-06-13 09:19:57 +0700

    Giải quyết vấn đề nhiều file index.js

---
+++ src/components/Button/Button.js
@@ -0,0 +1,76 @@
+import PropTypes from 'prop-types';
+import classNames from 'classnames/bind';
+import { Link } from 'react-router-dom';
+import styles from './Button.module.scss';
+
+const cx = classNames.bind(styles);
+
+function Button({
+    to,
+    href,
+    primary = false,
+    outline = false,
+    text = false,
+    rounded = false,
+    disabled = false,
+    small = false,
+    large = false,
+    children,
+    className, // custom riêng
+    leftIcon,
+    rightIcon,
+    onClick,
+    ...passProps
+}) {
+    let Component = 'button';
+    const props = {
+        onClick,
+        ...passProps,
+    };
+
+    // Remove event listeners when button is disabled
+    if (disabled) {
+        Object.keys(props).forEach((key) => {
+            if (key.startsWith('on') && typeof props[key] === 'function') {
+                delete props[key];
+            }
+        });
+    }
+
+    if (to) {
+        props.to = to;
+        Component = Link;
+    } else if (href) {
+        props.href = href;
+        Component = 'a';
+    }
+
+    const classes = cx('wrapper', { [className]: className, primary, outline, text, rounded, disabled, small, large });
+
+    return (
+        <Component className={classes} {...props}>
+            {leftIcon && <span className={cx('icon')}>{leftIcon}</span>}
+            <span className={cx('title')}>{children}</span>
+            {rightIcon && <span className={cx('icon')}>{rightIcon}</span>}
+        </Component>
+    );
+}
+
+Button.propTypes = {
+    to: PropTypes.string,
+    href: PropTypes.string,
+    primary: PropTypes.bool,
+    outline: PropTypes.bool,
+    text: PropTypes.bool,
+    rounded: PropTypes.bool,
+    disabled: PropTypes.bool,
+    small: PropTypes.bool,
+    large: PropTypes.bool,
+    children: PropTypes.node.isRequired,
+    class: PropTypes.string,
+    leftIcon: PropTypes.node,
+    rightIcon: PropTypes.node,
+    onClick: PropTypes.func,
+};
+
+export default Button;
